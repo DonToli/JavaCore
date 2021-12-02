61a9da5b82d11207d9e27679a5a64a3149daeb99
DonToli <strt87@yandex.ru>
2021-12-02 17:54:50 +0300

    ProduceConsumer

---
+++ src/WaitNotify.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+
+public class WaitNotify {
+    public static void main(String[] args) throws InterruptedException {
+        WaitandNotify wn = new WaitandNotify();
+        Thread thread1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    wn.produce();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        Thread thread2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    wn.consume();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        thread1.start();
+        thread2.start();
+
+        thread1.join();
+        thread2.join();
+    }
+}
+class WaitandNotify{
+    public void produce () throws InterruptedException {
+        synchronized (this){
+            System.out.println("Producer thread started...");
+            wait();
+            System.out.println("Producer thread resume...");
+        }
+
+    }
+
+    public void consume () throws InterruptedException {
+        Thread.sleep(2000);
+        Scanner scanner = new Scanner(System.in);
+
+        synchronized (this){
+            System.out.println("Waiting for return key pressed");
+            scanner.nextLine();
+            notify();
+        }
+
+    }
+
+}
